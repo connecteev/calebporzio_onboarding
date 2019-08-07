be04b97ea522642bf8f3006c901ede4bd7c2b6dc
Kunal Punjabi <kunalspunjabi@gmail.com>
2019-08-07 12:32:01 -0700

    add changes sugested by Caleb

--- app/User.php
+++ app/User.php
@@ -5,11 +5,11 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use \Calebporzio\Onboard\GetsOnboarded;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use \Calebporzio\Onboard\GetsOnboarded;
 
     /**
      * The attributes that are mass assignable.
